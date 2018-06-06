32c580e81055884da1be062afd5cdb2f32de1f4d
Clark Newell <williamcnewell@gmail.com>
2018-06-06 14:04:04 -0600

    adding image slot

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './registerServiceWorker'
+import Vuetify from 'vuetify'
+
+Vue.use(Vuetify)
 
 Vue.config.productionTip = false
 
